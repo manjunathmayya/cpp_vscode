3c5914ad4ac043e2c0938447c8c8a9b873eae42e
K <manjunath.k@siemens-healthineers.com>
2021-04-15 15:03:08 +0530

    checking in cpp_vscode folder

---
+++ cpp_vscode/stack.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+struct Node
+{
+    T data;
+    Node<T> * link;
+};
+
+template <class T>
+class Stack
+{
+    public:
+    Node<T> * top;
+
+    Stack()
+    {
+        top = NULL;
+    }
+
+    void push(T data)
+    {
+        Node<T> * temp = new Node<T>();
+        temp->data = data;
+        temp->link = NULL;
+
+        if (top == NULL)
+        {
+            top = temp;
+        }
+        else
+        {
+            temp->link = top;
+            top = temp;
+        }
+    }
+
+    T pop()
+    {
+        T data = top->data;
+        cout << "Pop              : "<< data << endl;
+
+        top = top->link;
+        return data;
+    }
+
+    void print()
+    {
+        Node<T> * temp = top;
+        while(temp != NULL)
+        {
+            cout << temp->data << "    ";
+            temp = temp->link;
+        }
+    }
+    
+    void print_reverse(Node<T> * node)
+    {
+        while(node == NULL)
+            return;
+        
+        print_reverse(node->link);
+        cout << node->data << "    ";        
+    }
+};
+
+int main()
+{
+    Stack<int> stack;
+    for(int index = 0; index < 15; index++)
+    {
+        stack.push(index);
+    }
+    stack.pop();
+    stack.pop();
+    cout << "Printing stack   : ";   stack.print();                    cout << endl;
+    cout << "Printing reverse : ";   stack.print_reverse(stack.top);   cout << endl;
+}
